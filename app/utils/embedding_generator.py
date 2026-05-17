61e21c22cfb3435c610db818bc25200b262cae33
Krishna7171 <krishnagupta96321@gmail.com>
2026-05-17 18:17:40 +0530

    Reduce embedding model memory usage

--- app/utils/embedding_generator.py
+++ app/utils/embedding_generator.py
@@ -7,7 +7,7 @@ def load_model():
     global model
 
     if model is None:
-        model = SentenceTransformer("all-MiniLM-L6-v2")
+        model = SentenceTransformer("paraphrase-MiniLM-L3-v2")
 
     return model
 
--- app/utils/llm_generator.py
+++ app/utils/llm_generator.py
@@ -2,10 +2,8 @@ from google import genai
 from dotenv import load_dotenv
 import os
 
-# Load environment variables
 load_dotenv()
 
-# Create Gemini client
 client = genai.Client(
     api_key=os.getenv("GEMINI_API_KEY")
 )
